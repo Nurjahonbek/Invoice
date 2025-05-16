e9ce92d3d08574f812269dc2988a74f63d8f0882
Nurjahonbek <nurjahonbekweb@gmail.com>
2025-05-16 15:41:03 +0500

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,15 +27,14 @@ function Sidebar() {
   return (
     <div className='bg-[#373B53] flex md:flex-col md:h-full md:z-[999] md:fixed md:left-0 md:top-0 md:w-[90px] md:bottom-0 items-center justify-between '>
         <img className="h-[90px] w-[90px] " src={logo} />
-      <div className='mr-6 md:flex-col md:mr-0 flex items-center md:mb-5'>
+      <div className='mr-6 max-sm:mr-0 md:flex-col max-sm:py-2 max-sm:flex-col md:mr-0 flex  items-center md:mb-5'>
         <ThemesToggle/>
-      <Avatar>
+      <Avatar className='max-sm:ml-40 max-sm:mt-2'>
       <AvatarImage src={oval} />
       <AvatarFallback>CN</AvatarFallback>
      </Avatar>
       </div>
       <Sheet open={sheetOpen} onOpenChange = {setSheetOpen} >
-
         <SheetContent className="ml-[72px] max-md:ml-0 max-md:max-w-[100%-90px] max-sm:ml-0 max-sm:min-w-[100%] min-w-[calc(70%-12px)] min-sm:overflow-x-hidden min-h-[calc(100%-56px)] overflow-y-scroll"  side="left">
           <SheetHeader className='sticky top-0 w-full bg-white border-b'>
             <SheetTitle className='text-[#0C0E16] text-2xl font-bold ml-7'>New Invoice</SheetTitle>
@@ -43,7 +42,7 @@ function Sidebar() {
           </SheetHeader>
         </SheetContent>
       </Sheet>
-    </div>
+      </div>
   )
 }
 
